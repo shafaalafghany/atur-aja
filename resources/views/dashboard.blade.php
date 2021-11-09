d7f752c2a809387e1132b60779f821d870cd8ff2
AriSyuhada <4r1syuhada@gmail.com>
2021-11-09 10:16:32 +0700

    add tailwind for css framework

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body class="bg-gray-200">
+    <div> <!-- content wrapper beginning -->
+        <div class="bg-white">
+            <nav class="grid grid-cols-5">
+                <div>
+                    <h1>
+                        <a href="/" class="">Atur Aja</a>
+                    </h1>
+                </div>
+                <div class="col-span-3">
+                    <ul class="flex">
+                        <li class="inline-flex">
+                            <a href="#">
+                                <span>Home</span>
+                            </a>
+                        </li>
+                        <li class="inline-flex">
+                            <a href="#">
+                                <span>About</span>
+                            </a>
+                        </li>
+                        <li class="inline-flex">
+                            <a href="#">
+                                <span>Contact</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="">
+                    <a href="#" class="justify-end">Login</a>
+                </div>
+            </nav>
+        </div>
+    </div> <!-- content wrapper end -->
+</body>
+</html>
