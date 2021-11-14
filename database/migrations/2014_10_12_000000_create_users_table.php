4ec4b6e964ffba537c6411bb106f712a7f231d6b
Shafa Alafghany <alafghanyshafa@gmail.com>
2021-11-14 23:21:18 +0700

    add new table for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->smallIncrements('user_id');
+            $table->string('user_fullname', 50);
+            $table->string('user_email', 100)->unique();
+            $table->string('user_password');
             $table->timestamps();
         });
     }
