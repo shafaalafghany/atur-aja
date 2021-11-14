58c0121250dd0c51b32dfdd4f3fda03ac84acd29
Shafa Alafghany <alafghanyshafa@gmail.com>
2021-11-14 23:23:30 +0700

    add new route for register

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
-Route::get('/login', function () {
-    return view('login');
-});
-Route::get('/register', function () {
-    return view('register');
-});
+
+//Auth
+Route::get('/register', [AuthController::class, 'signup']);
+Route::post('/auth/register', [AuthController::class, 'store']);
+Route::get('/login', [AuthController::class, 'signin']);
