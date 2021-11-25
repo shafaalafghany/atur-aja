35e6d3ad17502bee6877d5ff6d15315f581871fa
AriSyuhada <4r1syuhada@gmail.com>
2021-11-26 02:40:04 +0700

    polishing books and groups pages features

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -39,12 +39,12 @@
                     <a href="" class="w-full">
                         <div class="item-box">
                             <div class="item-icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z" />
                                 </svg>
                             </div>
                             <div class="flex-1 pl-1 mr-16">
-                                <div class="font-medium">Grup 1</div>
+                                <div class="font-medium">Buku Kas 1</div>
                                 <div class="text-gray-600 text-sm">Keterangan 1</div>
                             </div>
                             <div class="text-gray-600 text-xs">Dibuat pada 6:00 AM</div>
@@ -60,12 +60,12 @@
                     <a href="" class="w-full">
                         <div class="item-box">
                             <div class="item-icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z" />
                                 </svg>
                             </div>
                             <div class="flex-1 pl-1 mr-16">
-                                <div class="font-medium">Grup 2</div>
+                                <div class="font-medium">Buku Kas 2</div>
                                 <div class="text-gray-600 text-sm">Keterangan 2</div>
                             </div>
                             <div class="text-gray-600 text-xs">Dibuat pada 10:00 AM</div>
@@ -81,12 +81,12 @@
                     <a href="" class="w-full">
                         <div class="item-box">
                             <div class="item-icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z" />
                                 </svg>
                             </div>
                             <div class="flex-1 pl-1 mr-10">
-                                <div class="font-medium">Grup 3</div>
+                                <div class="font-medium">Buku Kas 3</div>
                                 <div class="text-gray-600 text-sm">Keterangan 3</div>
                             </div>
                             <div class="text-gray-600 text-xs">
